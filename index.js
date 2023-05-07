a684b6f32f2165743f3b5eec6863eddba79c93d6
Tram Anh Nguyen <anhng148@gmail.com>
2023-05-06 20:11:06 -0500

    add click button function

--- index.js
+++ index.js
@@ -1,6 +1,106 @@
-$(document).ready(function () {
+var currentInputTimeVal = "";
+var currentTimeIndex = 4;
 
+$(document).ready(function () {
+    
     $('.numBtn').on('click', function() {
-        $('#timeInput').value = this.value;
+        var currentButtonVal = $(this).text();
+        currentInputTimeVal = $("#timeInput").val();
+
+        if (currentInputTimeVal == "") {
+            currentInputTimeVal = $("#timeInput").attr("placeholder");
+        }
+        t = currentInputTimeVal.split("");
+        console.log(currentTimeIndex);
+        switch (currentTimeIndex) {
+            case 4:
+                t[4] = currentButtonVal;
+                currentTimeIndex--;
+                outputTimeText(t);
+                break;
+            case 3:
+                t[3] = t[4];
+                t[4] = currentButtonVal;
+                if (combineTwoNumber(t[3], t[4]) > 59) {
+                    t[3] = 0;
+                }
+
+                currentTimeIndex--;
+                outputTimeText(t);
+                break;
+            case 2:
+                t[1] = t[3];
+                t[3] = t[4];
+                t[4] = currentButtonVal;
+                currentTimeIndex--;
+                outputTimeText(t);
+                break;
+            case 1:
+                t[0] = t[1];
+                t[1] = t[3];
+                t[3] = t[4];
+                t[4] = currentButtonVal;
+                currentTimeIndex--;
+                outputTimeText(t);
+                break;
+            default:
+                $("#timeInput").val("00:00");
+                currentTimeIndex = 4;
+        }
+    });
+
+    $('#backSpaceButton').on('click', function() {
+        currentInputTimeVal = $("#timeInput").val();
+
+        if (currentInputTimeVal == "") {
+            currentInputTimeVal = $("#timeInput").attr("placeholder");
+        }
+
+        t = currentInputTimeVal.split("");
+        console.log(currentTimeIndex);
+
+        switch (currentTimeIndex) {
+            case 3:
+                t[4] = 0;
+                currentTimeIndex++;
+                outputTimeText(t);
+                break;
+            case 2:
+                t[4] = t[3];
+                t[3] = t[1];
+                currentTimeIndex++;
+                outputTimeText(t);
+                break;
+            case 1:
+                t[3] = t[1];
+                t[1] = t[0];
+                t[0] = 0;
+                currentTimeIndex++;
+                outputTimeText(t);
+                break;
+            case 0:
+                t[4] = t[3];
+                t[3] = t[1];
+                t[1] = t[0];
+                t[0] = 0;
+                currentTimeIndex++;
+                outputTimeText(t);
+                break;
+            // default:
+            //     $("#timeInput").val("00:00");
+            //     currentTimeIndex = 4;
+        }
     });
 });
+
+function combineTwoNumber(a, b) {
+    return parseInt(a + b);
+}
+
+function outputTimeText(t) {
+    var res = "";
+    t.forEach(element => {
+        res += element;
+    });
+    $("#timeInput").val(res);
+}
